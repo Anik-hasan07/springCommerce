1f60d26b7be1fc1126806a06949c503e1cffa58d
Anik-hasan07 <iamakashcse@gmail.com>
2023-09-24 18:55:25 +0600

    feature/your profile

--- src/features/navbar/Navbar.js
+++ src/features/navbar/Navbar.js
@@ -13,7 +13,7 @@ const user = [
   { name: 'Team', href: '#', current: false },
 ]
 const userNavigation = [
-  { name: 'Your Profile', link: '/profile' },
+  { name: 'My Profile', link: '/profile' },
   { name: 'My Orders', link: '/orders' },
   { name: 'Sign out', link: '/signout' },
 ];
